edaf316b0daeccc6144b02678a7e7d139d93be6f
MvdS <git@vandersluys.nl>
2020-10-02 11:50:53 +0200

    Initial commit: package skeleton.

---
+++ README.md
@@ -0,0 +1,31 @@
+# AstroTool #
+
+A Python package for astronomical calculations in Python or on the command line.
+
+
+## Installation ##
+
+This package can be installed using `pip install astrotool`.  This should automatically install the
+dependency packages `numpy` and `fortranformat` if they haven't been installed already.  If you are installing
+by hand, you have to ensure these packages are installed as well.
+
+
+## AstroTool pages ##
+
+* [Radboud University](http://astro.ru.nl/~sluys/AstroTool/): AstroTool homepage, data files
+* [Pypi](https://pypi.org/project/astrotool/): AstroTool Python package
+* [GitHub](https://github.com/MarcvdSluys/AstroTool): AstroTool source code
+
+
+## Author and licence ##
+
+* Author: Marc van der Sluys
+* Contact: http://astro.ru.nl/~sluys/
+* Licence: [GPLv3+](https://www.gnu.org/licenses/gpl.html)
+
+
+## References ##
+
+* Meeus, [Astronomical algorithms](https://www.willbell.com/math/MC1.HTM), 2nd Ed.
+* [Celestial mechanics in a nutshell (CMiaNS)](https://cmians.sourceforge.io/)
+* This Python code is adapted from the Fortran implementation in [libTheSky](http://libthesky.sourceforge.net/)
---
+++ astrotool/__init__.py
@@ -0,0 +1,27 @@
+#  Copyright (c) 2020  Marc van der Sluys - marc.vandersluys.nl
+#   
+#  This file is part of the AstroTool Python package,
+#  see: http://astro.ru.nl/~sluys/AstroTool/
+#   
+#  This is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+#  
+#  This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+#  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License along with this code.  If not, see
+#  <http://www.gnu.org/licenses/>.
+
+
+"""AstroTool module
+
+AstroTool is a Python package to do basic astronomical calculations in Python or on the command line.  The
+package can be used under the conditions of the GPLv3 licence.  These pages contain the API documentation.
+For more information on the Python package, licence, source code and data files, see the [AstroTool
+homepage](http://astro.ru.nl/~sluys/AstroTool/).
+
+"""
+
+
+name = "astrotool"
+
