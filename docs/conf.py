b9d4aa4d83d670dbca2e4a7c343b2801b8762d54
MvdS <git@vandersluys.nl>
2022-08-16 21:37:17 +0200

    docs/conf, setup: minor update doc.

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath('../'))
 # -- Project information -----------------------------------------------------
 
 project = 'astrotool'
-copyright = '2021, Marc van der Sluys'
+copyright = '2021-2022, Marc van der Sluys'
 author = 'Marc van der Sluys'
 
 
