447686bc040ff31a49d49ca34d3b74fd02bd1361
MvdS <git@vandersluys.nl>
2023-07-23 15:41:32 +0200

    Update copyright.

--- astrotool/coordinates.py
+++ astrotool/coordinates.py
@@ -2,14 +2,14 @@
 # -*- coding: utf-8 -*-
 # SPDX-License-Identifier: EUPL-1.2
 
-#  Copyright (c) 2019-2022  Marc van der Sluys - marc.vandersluys.nl
+#  Copyright (c) 2019-2023  Marc van der Sluys - marc.vandersluys.nl
 #   
 #  This file is part of the AstroTool Python package,
 #  see: http://astro.ru.nl/~sluys/AstroTool/
 #   
-#  AstroTool has been developed by Marc van der Sluys of the Department of Astrophysics at the Radboud
-#  University Nijmegen, the Netherlands and the department of Sustainable energy of the HAN University of
-#  applied sciences in Arnhem, the Netherlands.
+#  AstroTool has been developed by Marc van der Sluys of the Department of Physics at Utrecht
+#  University, the Netherlands and the Netherlands Institute for Nuclear and High-Energy Pysics (Nikhef)
+#  in Amsterdam.
 #   
 #  This is free software: you can redistribute it and/or modify it under the terms of the
 #  European Union Public Licence 1.2 (EUPL 1.2).
